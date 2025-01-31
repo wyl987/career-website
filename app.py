d46682613db4610114f540f92ae8360249850a13
wyl987 <wyl987@uowmail.edu.au>
2025-01-31 15:54:10 +1100

    Added jobitem page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 flask_app = Flask(__name__)
 
@@ -13,6 +13,14 @@ def hello_world():
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+@flask_app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+  return render_template('jobpage.html',
+                         job = job)
 
 if __name__ == "__main__":
   flask_app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -16,3 +16,17 @@ def load_jobs_from_db():
       jobs.append(row_dict)
   
   return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("select * from jobs where id = :val"),
+      {"val": id}
+      )
+    row = result.fetchone()
+    if row:
+      job = row._asdict()
+    else:
+      job = None
+  
+  return job
